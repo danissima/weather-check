a2f729acbcc5686008e69273340118667fc04787
Danila Odintsov <danila.odintsov@nutnet.ru>
2021-08-06 18:05:39 +0400

    single city page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,14 +5,14 @@ import DefaultCities from '../../components/DefaultCities/DefaultCities'
 import { apiKey } from '../../assets/ts/apiKey'
 
 const Home: React.FC = () => {
-  let xml = new XMLHttpRequest()
-  xml.open('GET', `https://api.openweathermap.org/data/2.5/weather?lang=ru&q=Ижевск&units=metric&appid=${apiKey}`)
-  xml.responseType = 'json'
+  // let xml = new XMLHttpRequest()
+  // xml.open('GET', `https://api.openweathermap.org/data/2.5/weather?lang=ru&q=Ижевск&units=metric&appid=${apiKey}`)
+  // xml.responseType = 'json'
   
-  xml.onload = () => {
-    console.log(xml.response)
-  }
-  xml.send()
+  // xml.onload = () => {
+  //   console.log(xml.response)
+  // }
+  // xml.send()
 
   return (
     <div className="Home">
--- src/pages/SingleCity/SingleCity.tsx
+++ src/pages/SingleCity/SingleCity.tsx
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { ReactComponent as Cloud } from '../../assets/images/WeatherTypes/Sun-cloud-mid-rain.svg'
 import { ReactComponent as Barometer } from '../../assets/images/barometer.svg'
 import Icon from "../../components/Icon/Icon";
+import { useParams } from "react-router-dom";
+import { apiKey } from "../../assets/ts/apiKey";
+
+interface pathParamsType {
+  city: string;
+}
+
+interface weatherResult {
+  name: string;
+  weather: {
+    description: string;
+  }[];
+  main: {
+    temp: number;
+    pressure: number;
+  }
+}
 
 const SingleCity: React.FC = () => {
+  const pathParams = useParams<pathParamsType>()
+  const [weatherNow, setWeatherNow] = useState<weatherResult>()
+
+  const getWeatherNow = useCallback(() => {
+    let xml = new XMLHttpRequest()
+    xml.open('GET', `https://api.openweathermap.org/data/2.5/weather?lang=ru&q=${pathParams.city}&units=metric&appid=${apiKey}`)
+    xml.responseType = 'json'
+    xml.onload = () => {
+      setWeatherNow(xml.response)
+      console.log(xml.response)
+    }
+    xml.send()
+  }, [pathParams.city])
+
+  useEffect(() => {
+    getWeatherNow()
+  }, [getWeatherNow])
+
   return (
     <div className="SingleCity">
-      <h2 className="SingleCity__title">Москва</h2>
-      <h3 className="SingleCity__subtitle">Облачно с прояснениями</h3>
-      <div className="SingleCity__info">
-        <p className="SingleCity__degrees">-13°</p>
-        <div className="SingleCity__image">
-          <Cloud />
-        </div>
-      </div>
-      <Icon text="756 мм рт. ст.">
-        <Barometer />
-      </Icon>
-      <p className="SingleCity__sun">Закат в 18:00</p>
+      {weatherNow &&
+        <>
+          <h2 className="SingleCity__title">{weatherNow.name}</h2>
+          <h3 className="SingleCity__subtitle">{weatherNow.weather[0].description}</h3>
+          <div className="SingleCity__info">
+            <p className="SingleCity__degrees">{`${Math.round(weatherNow.main.temp)}°`}</p>
+            <div className="SingleCity__image">
+              <Cloud />
+            </div>
+          </div>
+          <Icon text={`${weatherNow.main.pressure} мм рт. ст.`}>
+            <Barometer />
+          </Icon>
+          <p className="SingleCity__sun">Закат в 18:00</p>
+        </>
+      }
     </div>
   )
 }
